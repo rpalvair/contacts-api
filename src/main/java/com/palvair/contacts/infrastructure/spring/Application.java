fabb13a77568ab1536f13d2595f5fc970304409b
ruddy palvair <r.palvair@gmail.com>
2021-04-19 20:56:33 +0200

    feat: Run application

---
+++ src/main/java/com/palvair/contacts/infrastructure/spring/Application.java
@@ -0,0 +1,10 @@
+package com.palvair.contacts.infrastructure.spring;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfiguration.class);
+    }
+}
