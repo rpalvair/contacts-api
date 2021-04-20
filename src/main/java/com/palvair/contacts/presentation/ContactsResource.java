c17f4ae001360cb07deb5d54d6bf2a5f61757977
ruddy palvair <r.palvair@gmail.com>
2021-04-20 20:29:33 +0200

    feat : Enable cross origin

--- src/main/java/com/palvair/contacts/presentation/ContactsResource.java
+++ src/main/java/com/palvair/contacts/presentation/ContactsResource.java
@@ -3,11 +3,13 @@ package com.palvair.contacts.presentation;
 import com.palvair.contacts.domain.Contact;
 import com.palvair.contacts.domain.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins="*")
 @RestController
 public class ContactsResource {
 
