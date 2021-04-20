df0aec5ee05198cb1694b103d81dc1846c31399f
ruddy palvair <r.palvair@gmail.com>
2021-04-20 11:41:05 +0200

    feat: Display contact by using the repository

--- src/main/java/com/palvair/contacts/presentation/ContactsResource.java
+++ src/main/java/com/palvair/contacts/presentation/ContactsResource.java
@@ -1,6 +1,8 @@
 package com.palvair.contacts.presentation;
 
 import com.palvair.contacts.domain.Contact;
+import com.palvair.contacts.domain.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,11 @@ import java.util.List;
 @RestController
 public class ContactsResource {
 
+    @Autowired
+    private ContactRepository contactRepository;
+
     @GetMapping("/contacts")
     public List<Contact> all() {
-        return Collections.emptyList();
+        return contactRepository.findAll();
     }
 }
