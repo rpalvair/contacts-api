a4a26ec01e0c1f457222a4fa071c95235ac51bee
ruddy palvair <r.palvair@gmail.com>
2021-04-21 10:41:12 +0200

    feat: Enable cross origin request for DELETE

--- src/main/java/com/palvair/contacts/presentation/DeleteContactResource.java
+++ src/main/java/com/palvair/contacts/presentation/DeleteContactResource.java
@@ -5,10 +5,12 @@ import com.palvair.contacts.domain.ContactRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="*")
 @RestController
 public class DeleteContactResource {
 
