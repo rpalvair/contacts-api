dc454c550791194b48028112cafab5259ee9523f
ruddy palvair <r.palvair@gmail.com>
2021-04-21 12:36:12 +0200

    feat : Endpoint to get one contact by id

--- src/main/java/com/palvair/contacts/presentation/ContactsResource.java
+++ src/main/java/com/palvair/contacts/presentation/ContactsResource.java
@@ -1,15 +1,17 @@
 package com.palvair.contacts.presentation;
 
+import com.palvair.contacts.application.exceptions.ContactNotFoundException;
 import com.palvair.contacts.domain.Contact;
 import com.palvair.contacts.domain.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 @RestController
 public class ContactsResource {
 
@@ -19,5 +21,10 @@ public class ContactsResource {
     @GetMapping("/contacts")
     public List<Contact> all() {
         return contactRepository.findAll();
+    }
+
+    @GetMapping("/contacts/{id}")
+    public Contact getOne(@PathVariable String id) {
+        return contactRepository.findById(id).orElseThrow(() -> new ContactNotFoundException(String.format("Contact not found with id %s", id)));
     }
 }
